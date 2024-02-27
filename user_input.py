4e7ec15dcb10d3a797e9d7c54a44d6721258a71b
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-27 08:20:41 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+age = input("How old are your? ")
+location = input("Where do you live? ")
+print(f"Hey {name}. You are {age} and you live in {location}.")
